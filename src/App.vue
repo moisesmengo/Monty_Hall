101ed5d8f24bf2fd695b6dde2b88df833e5f7892
moisesmengo <moisesmengo@live.com>
2020-04-10 19:51:55 -0300

    criando o componente presente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
+        <Gift/>
     </div>
 </template>
 <script>
+import Gift from './components/Gift'
 export default {
-    
+    name: 'App',
+    components: { Gift }
 }
 </script>
 <style>
